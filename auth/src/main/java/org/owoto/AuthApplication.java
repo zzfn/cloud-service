41b541af54fdf87430d7c55e068ac535f4577852
chenc <chenc@fengyuntec.com>
2020-12-08 17:09:01 +0800

    fix: 启动报错

--- auth/src/main/java/org/owoto/AuthApplication.java
+++ auth/src/main/java/org/owoto/AuthApplication.java
@@ -2,13 +2,14 @@ package org.owoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zzfn
  * @date 2020-12-07 11:42 上午
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 public class AuthApplication {
     public static void main(String[] args) {
--- common/src/main/java/org/owoto/CommonApplication.java
+++ common/src/main/java/org/owoto/CommonApplication.java
@@ -2,8 +2,9 @@ package org.owoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class CommonApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
