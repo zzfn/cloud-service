92f012f3d323f2b1e1c9ba60cadb832b80f9df3b
zzfn <chenc@fengyuntec.com>
2020-12-08 21:18:38 +0800

    feat: 测试网关

--- auth/src/main/java/org/owoto/controller/TestController.java
+++ auth/src/main/java/org/owoto/controller/TestController.java
@@ -1,6 +1,8 @@
 package org.owoto.controller;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(tags="测试")
 public class TestController {
+    @Autowired
+    private Environment environment;
     @GetMapping("test")
     public Object test(){
-        return "hhhh";
+        return environment.getProperty("server.port");
     }
 }
