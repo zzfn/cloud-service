4bac2f4d66669af19bc67ccad9ac0fb14436c689
chenc <chenc@fengyuntec.com>
2020-12-18 21:22:31 +0800

    fix: test

--- article/src/main/java/org/owoto/controller/ArticleESController.java
+++ article/src/main/java/org/owoto/controller/ArticleESController.java
@@ -1,16 +1,22 @@
 package org.owoto.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.owoto.mapper.ArticleESDao;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.owoto.entity.ArticleES;
+import org.owoto.mapper.ArticleESDao;
 import org.owoto.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author zzfn
@@ -18,6 +24,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("es")
+@Slf4j
 public class ArticleESController {
     @Autowired
     private ArticleESDao articleESDao;
@@ -40,9 +47,14 @@ public class ArticleESController {
         queryBuilder.should(QueryBuilders.matchPhraseQuery("title",keyword))
                 .should(QueryBuilders.matchPhraseQuery("content",keyword))
                 .should(QueryBuilders.matchPhraseQuery("tag_desc",keyword));
-        Iterable<ArticleES> pages=articleESDao.search(queryBuilder);
-        List<ArticleES> list=new ArrayList<>();
-        pages.forEach(list::add);
+        NativeSearchQuery nativeSearchQuery=new NativeSearchQueryBuilder().withQuery(queryBuilder).withHighlightBuilder(new HighlightBuilder().field("title").field("content").field("tag_desc")).build();
+//        Iterable<ArticleES> pages=articleESDao.search(nativeSearchQuery);
+//        Iterable<ArticleES> pages=articleESDao.search(queryBuilder);
+        List<Map> list=new ArrayList<>();
+       SearchHits<ArticleES> articleES= elasticsearchRestTemplate.search(nativeSearchQuery,ArticleES.class);
+       articleES.forEach(articleESSearchHit -> {
+           list.add(articleESSearchHit.getHighlightFields());
+       });
         return ResultUtil.success(list);
     }
 }
