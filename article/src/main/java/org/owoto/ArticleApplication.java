92d3bd278ca656a155c6dd9759d68303557834c6
chenc <chenc@fengyuntec.com>
2020-12-30 14:07:28 +0800

    fix: 改用nacos

--- article/src/main/java/org/owoto/ArticleApplication.java
+++ article/src/main/java/org/owoto/ArticleApplication.java
@@ -3,14 +3,14 @@ package org.owoto;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author zzfn
  * @date 2020-12-07 2:12 下午
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan("org.owoto.mapper")
 public class ArticleApplication {
     public static void main(String[] args) {
--- gateway/src/main/java/org/owoto/GatewayApplication.java
+++ gateway/src/main/java/org/owoto/GatewayApplication.java
@@ -2,14 +2,14 @@ package org.owoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author zzfn
  * @date 2020-12-07 11:02 上午
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
