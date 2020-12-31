77d939abfbdf694aea338e1c60e469616f1e6a32
chenc <chenc@fengyuntec.com>
2020-12-31 16:50:05 +0800

    feat: 整合openfeign

--- search/src/main/java/org/owoto/controller/ArticleController.java
+++ search/src/main/java/org/owoto/controller/ArticleController.java
@@ -5,8 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.owoto.entity.ArticleES;
-import org.owoto.mapper.ArticleESDao;
+import org.owoto.entity.Article;
 import org.owoto.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
@@ -15,36 +14,19 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author zzfn
- * @date 2020-12-10 14:58
+ * @date 2020-12-31 3:35 下午
  */
 @RestController
-@RequestMapping("es")
+@RequestMapping("white/article")
 @Slf4j
-public class ArticleESController {
-    @Autowired
-    private ArticleESDao articleESDao;
+public class ArticleController {
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
-
-    @PostMapping("test")
-    public Object listTags(@RequestBody ArticleES articleES) {
-        return ResultUtil.success(articleESDao.save(articleES));
-    }
-
-    @PostMapping("del")
-    public Object del() {
-        articleESDao.deleteAll();
-        elasticsearchRestTemplate.deleteIndex("article");
-        return ResultUtil.success(null);
-    }
 
     @GetMapping("list")
     public Object getList(String keyword) {
@@ -53,11 +35,11 @@ public class ArticleESController {
                 .should(QueryBuilders.matchPhraseQuery("content", keyword))
                 .should(QueryBuilders.matchPhraseQuery("tag_desc", keyword));
         NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder().withQuery(queryBuilder).withHighlightBuilder(new HighlightBuilder().field("title").field("content").field("tag_desc")).build();
-        List<ArticleES> list = new ArrayList<>();
-        SearchHits<ArticleES> articleES = elasticsearchRestTemplate.search(nativeSearchQuery, ArticleES.class);
+        List<Article> list = new ArrayList<>();
+        SearchHits<Article> articleES = elasticsearchRestTemplate.search(nativeSearchQuery, Article.class);
         articleES.getSearchHits().forEach(searchHit -> {
-            ArticleES articleES1 = searchHit.getContent();
-            articleES1.setContent(StringUtils.join(searchHit.getHighlightField("content")," "));
+            Article articleES1 = searchHit.getContent();
+            articleES1.setContent(StringUtils.join(searchHit.getHighlightField("content"), " "));
             if (searchHit.getHighlightField("tagDesc").size() != 0) {
                 articleES1.setTagDesc(StringUtils.join(searchHit.getHighlightField("tagDesc"), " "));
             }
@@ -67,5 +49,18 @@ public class ArticleESController {
             list.add(articleES1);
         });
         return ResultUtil.success(list);
+    }
+
+    @DeleteMapping("{id}")
+    public Object deleteArticle(@PathVariable("id") String id) {
+        log.error("sadasd");
+        return "dasdasd";
+//        return elasticsearchRestTemplate.delete(id, Article.class);
+    }
+    @GetMapping("{id}")
+    public Object test(@PathVariable("id") String id) {
+        log.error("sadasd");
+        return id;
+//        return elasticsearchRestTemplate.delete(id, Article.class);
     }
 }
